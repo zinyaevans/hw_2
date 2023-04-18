4c843b6bc69676d0be7b41da99e613e43ddfa6e3
Nadya <prokopyevans@gmail.com>
2023-04-18 12:03:32 +0500

    new function rememberMyFilms

--- js/script.js
+++ js/script.js
@@ -47,18 +47,22 @@ const personalMovieDB ={
 };
 
 
-for(let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-    
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
+
+rememberMyFilms();
 
 if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов');
