155126b520821065a5ae3469f840a14cae2605b4
Nadya <prokopyevans@gmail.com>
2023-04-18 12:13:33 +0500

    new function detectPersonalLevel

--- js/script.js
+++ js/script.js
@@ -64,15 +64,19 @@ function rememberMyFilms() {
 
 rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
 
