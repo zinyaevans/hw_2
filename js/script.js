428a5470c4c93c2a9847a079b55f480c9e2174b2
Nadya <prokopyevans@gmail.com>
2023-04-20 12:03:17 +0500

    method trim()

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ const personalMovieDB ={
 
 function rememberMyFilms() {
     for(let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(), //Метод trim() удаляет пробельные символы с начала и конца строки
               b = prompt('На сколько оцените его?', '');
         
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
